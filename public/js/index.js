9e1a5837f059d8b4d595d3541a286b552010e976
Andrey <andreyorlov33@gmail.com>
2017-03-06 01:28:07 -0800

    papa parser is set up, further config needed, server payload limit needs to be set to accomadate file size, database needs to be set up for testing

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,61 @@
 $(document).ready(()=>{
-    console.log('ready');
 
-  $('#fileSubmit').submit((event)=>{
-      event.preventDefault();
-      let file = $('#file').fileTransfer.files[0];
-     console.log(file)
+    let dropZone = document.getElementById('dropzone');
+
+    function sendDataToServer(data){
+      $.ajax({
+            url: '/api/parse',
+            type: 'POST',
+            data: data,
+            processData: false,
+            contentType: false,
+            success: function(data){
+                console.log('upload successful!');
+            }
+        });
+    }   
+
+    const parse = (file) => {
+        Papa.parse(file, {
+         
+            delimiter: "",	// auto-detect
+            newline: "",	// auto-detect
+            header: true,
+            dynamicTyping: false,
+            preview: 0,
+            encoding: "",
+            worker: false,
+            comments: false,
+            step: undefined,
+            complete: undefined,
+            error: undefined,
+            download: true,
+            skipEmptyLines: false,
+            chunk: undefined,
+            fastMode: undefined,
+            beforeFirstChunk: undefined,
+            withCredentials: undefined,
+     // complete call back =============================
+            complete: function(results) {
+                data = results.data;
+                sendDataToServer(data);
+             }
+        });
+    }
+
+    dropZone.ondrop = (e)=>{
+        e.preventDefault();
+        parse(e.dataTransfer.files[0])
+
+    }
     
-  })
+    dropZone.ondragover = ()=>{
+        this.className = 'dragover';
+        return false;
+    }
+
+       dropZone.ondragleave = ()=>{
+        this.className = 'dropzone';
+        return false;
+    }
 })
