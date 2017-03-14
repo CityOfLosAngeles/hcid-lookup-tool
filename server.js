287bcb945146114e906f09fd60a8ab0a7aa472f4
Matt Whitcomb <mwhitcom@gmail.com>
2017-03-13 20:31:15 -0700

    added function to create uploads and temp-data folders should they not initially exist in directory

--- server.js
+++ server.js
@@ -7,40 +7,58 @@ const multer = require('multer');
 const db = require('./models');
 
 const PORT = process.env.PORT || 6060;
-const app = express();
 
-app.use(router);
 
 // ************** MULTER CONFIG ****************** // 
 const storage = multer.diskStorage({
-     destination: function (req, file, cb) {
-         cb(null, 'uploads')
+    destination: function (req, file, cb) {
+        cb(null, 'uploads')
     },
     filename: function (req, file, cb) {
         cb(null, `${file.originalname}-${Date.now()}`)
-     }
-})
+    }
+});
 
 const upload = multer({storage: storage}); // PASS THE CONFIG INTO MULTER
 // *************************************************// 
 
 router.get('/', (req, res) => {
-	 res.sendFile(path.join(__dirname, './public/index.html'))
-})
+    checkFolder('uploads');
+    checkFolder('temp-data');
+    res.sendFile(path.join(__dirname, './public/index.html'))
+});
 
 router.post('/upload', upload.single('file'), (req, res) => {
     watchFolder();
 	res.status(200).redirect('/');
 });
 
-app.use((req, res, next) => {
-    res.setTimeout(480000, () => { // 4 minute timeout adjust for larger uploads
+let app = express();
+
+app.use(router);
+app.use(function(req, res, next){
+    res.setTimeout(480000, function(){ // 4 minute timeout adjust for larger uploads
         console.log('Request has timed out.');
             res.send(408);
         });
+
     next();
 });
 
+db.sequelize.sync().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server listening on port ${PORT}`);
+    });
+});
+
+
+// Functions Section
+
+checkFolder = (folderName) => {
+    if (!fs.existsSync(`./${folderName}`)){
+        fs.mkdirSync(`./${folderName}`);
+    }
+}
 
 watchFolder = () => {
     fs.watch('./uploads', (eventType, filename) => {
@@ -48,8 +66,8 @@ watchFolder = () => {
             console.log(eventType, filename);
             copyToTemp(filename);
         }
-        if (!filename){
-            return;
+        if(!filename){
+            return
         }
     });
 }
@@ -65,11 +83,5 @@ copyToTemp = (src) => {
         console.log('done copying');
     });
     
-    readStream.pipe(fs.createWriteStream('./temp-data/${src}'));
+    readStream.pipe(fs.createWriteStream(`./temp-data/${src}`));
 }
-
-db.sequelize.sync().then(() => {
-    app.listen(PORT, () => {
-        console.log(`Server listening on port ${PORT}`);
-    });
-});
