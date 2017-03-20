fc0012e7a710a8d400564838f585568f4d5c789f
Matt Whitcomb <mwhitcom@gmail.com>
2017-03-20 10:18:33 -0700

    Completed parsing step to create addressmaster batch for bims file, added parse-address to package.json

--- controllers/bims_controller.js
+++ controllers/bims_controller.js
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import csv from 'fast-csv';
+import addressParser from 'parse-address';
 
 module.exports = {
     readData: (app) => {
@@ -37,54 +38,113 @@ module.exports = {
         }
 
         // CLEANS EACH LINE, Replaces unwated , with spaces, replaces "," with , and removes " before and after each string
-        let cleanUpLine = (data)=> {
+        function cleanUpLine(data){
                 let line = data.toString();
                 let cleanLine = line
                     .replace(/, /g, ' ')
                     .replace(/","/g, ',')
                     .replace(/"/g, '')
+                    .replace(/&/g, ' ')
                     .split(',')
                return cleanLine
         }
 
         let csvStream = csv({quote: null})
             .on("data", function(data){
-  
-                // runAddressMaster(data)
-                runRawData(cleanUpLine(data));
+                runConstructors(cleanUpLine(data));
             })
             .on("end", function(){
                 // Last batch DB function goes here
                 console.log("done");
             });
-            
-        let runAddressMaster = (readableStream) => {
-            let rawPropAddress = readableStream[4].trim();
-            let rawCityStateZip = readableStream[5].trim();
-            let tempAddress = new AddressMaster();
+        
+        function runRawData(readableStream){
+            let tempData = new RawData(...readableStream);
+            rawBatch.push(tempData); 
         }
 
-        let runRawData = (readableStream) => {
-            let tempData = new RawData(...readableStream);
-            rawBatch.push(tempData);
-            if(rawBatch.length % batchSize === 0){
+        function runAddressMaster(readableStream){
+            let rawPropAddress = readableStream[3];
+            let rawCityStateZip = readableStream[4];
+            let splitCityStateZip = rawCityStateZip.split(' ');
+            let splitPropAddress = rawPropAddress.split(' ');
+
+            // Gets Zipcode
+            let rawZipcode = splitCityStateZip.pop();
+
+            // Gets State
+            let rawState = splitCityStateZip.pop();
+
+            // Gets City
+            let rawCity = splitCityStateZip.join(' ');
+
+            // Parses Address using parse-address module
+            let addressObject = addressParser.parseLocation(rawPropAddress);
+
+            // Shaves off street number as first step to find apartment unit info 
+            let splitNum = splitPropAddress.indexOf(addressObject.number);
+            splitPropAddress.splice(splitNum, 1);
+
+            // If direction (North, South, East, West) is part of address, assigns it, otherwise makes it null
+            let addressDirection;
+            if(addressObject.prefix !== undefined){
+                addressDirection = addressObject.prefix;
+                splitPropAddress.shift();
+            } else {
+                addressDirection = null;
+            }
+
+            // Loop to shave off street name to find apartment unit info
+            let splitParsedName = addressObject.street.split(' ');
+            for (var i = 0, x = splitParsedName.length; i < x; i++) {
+                let splitName = splitPropAddress.indexOf(splitParsedName[i]);
+                splitPropAddress.splice(splitName, 1);
+            }
+
+            // Shaves off type of road to find apartment unit info
+            let splitType = splitPropAddress.indexOf(addressObject.type);
+            splitPropAddress.splice(splitType, 1);
+
+            // Aparment unit info is what's left in array, assigns it to addressUnit if it exists, otherwise makes it null
+            let addressUnit = splitPropAddress.join(' ');
+            if(!addressUnit){
+                addressUnit = null;
+            }
+
+            // Runs parsed data through AddressMaster contrustor and pushes it to the batch
+            let tempAddress = new AddressMaster(addressObject.number, addressObject.street, addressObject.type, addressDirection, addressUnit, rawCity, rawState, rawZipcode);
+            addressMasterBatch.push(tempAddress); 
+        }
+
+        function runConstructors(readableStream) {
+            runRawData(readableStream);
+            runAddressMaster(readableStream);
+
+            // Batch control: limits arrays to 1000 address objects, then runs DB functions and starts again
+            if(rawBatch.length % batchSize === 0 && rawBatch.length !== 0){
                 pause();
+                console.log(rawBatch[999]);
+                console.log(addressMasterBatch[999]);
+                // Function call for checking DB and seeding DB goes here
                 resume();
             }
         }
 
+        // Function to pause data stream from file
         function pause(){
             stream.unpipe(csvStream);
             return csvStream.pause();
         }
 
+        // Function to reset batches and resume data stream from file
         function resume(){
             rawBatch = [];
             addressMasterBatch = [];
             stream.pipe(csvStream);
             return csvStream.resume();
         } 
+
+        // Runs the data stream through the csv parser
         stream.pipe(csvStream);
     }
 }
-
