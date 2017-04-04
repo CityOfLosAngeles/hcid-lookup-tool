e137332118530af6bd0b8e6ab550c1047b8ffb21
hunterowens <owens.hunter@gmail.com>
2017-04-03 17:46:45 -0700

    inital load script

---
+++ etl/load.sh
@@ -0,0 +1,26 @@
+#!/bin/zsh
+
+# stop if error
+set -e 
+
+# Run CSVclean to clean the datasets
+for file in ./data/*; do
+  echo "cleaning: " $file
+  if [[ $file == *"prop_unit.csv"* ]]; then
+      echo "Prop Unit is bad"
+      continue 
+  fi
+  csvclean -v $file
+done
+
+# insert into postgres
+for clean_file in ./data/*_out.csv; do
+  echo "Inserting " $clean_file
+  csvsql --db postgresql:///hcid-disp --prefix REPLACE --insert $clean_file
+  echo "Finished " $clean_file
+done
+
+# remove output files
+rm ./data/*_out.csv
+rm ./data/*_err.csv
+
