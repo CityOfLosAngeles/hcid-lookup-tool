200c1386676d466377582d6021a8fc4562a30861
Andrey <andreyorlov33@gmail.com>
2017-04-27 21:50:44 -0700

    fixed visual bug wiht the select file button

--- controllers/hims_controller.js
+++ controllers/hims_controller.js
@@ -3,6 +3,7 @@ import csv from 'fast-csv';
 import addressParser from 'parse-address';
 import addressController from './address.js';
 
+
 module.exports = {
     readData: (app) => {
         let stream = fs.createReadStream("./temp-data/hims.csv");
@@ -10,21 +11,21 @@ module.exports = {
         let addressMasterBatch = [];
         let counter = 0;
 
-        function RawData(c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15, c16, c17, c18, c19, c20, c21, c22 , c23) {
-            this.HOUSING_PROGRAM = c1;
-            this.ProjUniqueID = c2;
-            this.ProjectNo = c3;
-            this.PROJECT_STATUS = c4;
-            this.PROJECT_INFO = c5;
-            this.APN = c6;
-            this.HouseId = c7;
-            this.HouseNum = c8;
-            this.HouseFracNum = c9;
-            this.PIN = c10; 
-            this.CouncilDistrict = c11;
-            this.PreDirCd = c12;
-            this.StreetName = c13;
-            this.StreetTypeCd = c14;
+        function RawData(c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15, c16, c17, c18, c19, c20, c21, c22, c23) {
+            this.HOUSING_PROGRAM = c1;
+            this.ProjUniqueID = c2;
+            this.ProjectNo = c3;
+            this.PROJECT_STATUS = c4;
+            this.PROJECT_INFO = c5;
+            this.APN = c6;
+            this.HouseId = c7;
+            this.HouseNum = c8;
+            this.HouseFracNum = c9;
+            this.PIN = c10;
+            this.CouncilDistrict = c11;
+            this.PreDirCd = c12;
+            this.StreetName = c13;
+            this.StreetTypeCd = c14;
             this.PostDirCd = c15;
             this.UnitRange = c16;
             this.Unit_Number = c17;
@@ -36,7 +37,7 @@ module.exports = {
             this.CensusTract = c23;
         }
 
-        function AddressMaster(a1, a2, a3, a4, a5, a6 , a7, a8){
+        function AddressMaster(a1, a2, a3, a4, a5, a6, a7, a8) {
             this.street_num = a1;
             this.street_name = a2;
             this.street_type = a3;
@@ -48,68 +49,66 @@ module.exports = {
         }
 
         // CLEANS EACH LINE, Replaces unwated , with spaces, replaces "," with , and removes " before and after each string
-        let cleanUpLine = (data)=> {
-                let line = data.toString();
-                let cleanLine = line
-                    .replace(/, /g, ' ')
-                    .replace(/","/g, ',')
-                    .replace(/"/g, '')
-                    .split(',')
-               return cleanLine
+        let cleanUpLine = (data) => {
+            let line = data.toString();
+            let cleanLine = line
+                .replace(/, /g, ' ')
+                .replace(/","/g, ',')
+                .replace(/"/g, '')
+                .split(',')
+            return cleanLine
         }
 
-        let csvStream = csv({quote: null})
-            .on("data", function(data){
-                console.log('\n$$$$$$$$$ Inside "on data" $$$$$$$$$');
-                if(!data[0].includes("HOUSING_PROGRAM")){
-                    console.log("^^^^^^ Inside if statement of 'on data' ^^^^^^");
-                    runConstructors( cleanUpLine(data) );
+        let csvStream = csv({ quote: null })
+            .on("data", function (data) {
+                if (!data[0].includes("HOUSING_PROGRAM")) {
+                    runConstructors(cleanUpLine(data));
                 }
             })
-            .on("end", function(){
+            .on("end", function () {
                 console.log("done");
             });
-        
-        function runRawData(readableStream){
-            let tempData = new RawData(...readableStream);
-            rawBatch.push(tempData);
-        }
-            
-        function runAddressMaster(readableStream){
+
+        function runAddressMaster(readableStream) {
             let state = 'CA';
 
             let street_num = readableStream[7];
-            if(!street_num){street_num = null;}
+            if (!street_num) { street_num = null; }
 
             let street_dir_cd = readableStream[11];
-            if(!street_dir_cd){street_dir_cd = null;}
+            if (!street_dir_cd) { street_dir_cd = null; }
 
             let street_name = readableStream[12];
-            if(!street_name){street_name = null;}
+            if (!street_name) { street_name = null; }
 
             let street_type = readableStream[13];
-            if(!street_type){street_type = null;}
+            if (!street_type) { street_type = null; }
 
             let street_unit = `${readableStream[15]} ${readableStream[16]}`;
-            if(street_unit === ' '){street_unit = null;}
+            if (street_unit === ' ') { street_unit = null; }
 
             let zipcode = readableStream[17];
-            if(!zipcode){zipcode = null;}
+            if (!zipcode) { zipcode = null; }
 
             let city = readableStream[18];
-            if(!city){city = null;}
+            if (!city) { city = null; }
 
             let tempAddress = new AddressMaster(street_num, street_name, street_type, street_dir_cd, street_unit, city, state, zipcode);
             addressMasterBatch.push(tempAddress);
         }
 
-       function runConstructors(readableStream) {
-            console.log('####### Inside Run Constructors #######');
-            runRawData(readableStream);
-            runAddressMaster(readableStream);
-            checkAddress( rawBatch[counter], addressMasterBatch[counter] );      
+        function runRawData(readableStream) {
+            let tempData = new RawData(...readableStream);
+            rawBatch.push(tempData);
         }
 
+        function runConstructors(readableStream) {
+            runRawData(readableStream);
+            runAddressMaster(readableStream);
+            checkAddress(rawBatch[counter], addressMasterBatch[counter]);
+        }
+
+        // function checkAddress(rawBatch, addressMasterBatch, callback ) {
         function checkAddress(rawBatchObject, addressMasterBatchObject) {
             console.log(`********************** Object Counter: ${counter} **********************`);
             console.log(addressMasterBatchObject);
@@ -117,16 +116,19 @@ module.exports = {
             pause3();
             addressController.createHims(addressMasterBatchObject, rawBatchObject, resume3);
         }
-        
-        function pause3(){
+
+        // Function to pause data stream from file
+        function pause3() {
             csvStream.pause();
-            stream.unpipe(csvStream);  
+            stream.unpipe(csvStream);
         }
 
-        function resume3(){
+        function resume3() {
             csvStream.resume();
             stream.pipe(csvStream);
         }
+        // Runs the data stream through the csv parser
         stream.pipe(csvStream);
     }
 }
+
