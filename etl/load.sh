40baf5c7e7445b88dd2a39abee7d1787850c95bb
hunterowens <owens.hunter@gmail.com>
2017-05-17 17:28:14 -0700

    load updates

--- etl/load.sh
+++ etl/load.sh
@@ -1,33 +1,17 @@
 #!/bin/zsh
 
-# stop if error
-set -e 
-
 DIR_NAME=$1
 
 # Run CSVclean to clean the datasets
-for file in ./$DIR_NAME/*; do
-  echo "cleaning: " $file
-  if [[ $file == *"prop_unit.csv"* ]]; then
-      echo "Prop Unit is bad"
-      continue 
-  fi
-  csvclean -v $file & # send to background for speed
-  echo "clean finishing for " $file
-done
-wait #until processes are done. 
+ls ./$DIR_NAME/* | parallel "csvclean -v {}" 
 
 # insert into postgres
-for clean_file in ./$DIR_NAME/*_out.csv; do
-  echo "Inserting " $clean_file
-  csvsql --db postgresql:///hcid-disp  --insert $clean_file
-  echo "Finished " $clean_file
-done
+ls ./$DIR_NAME/*_out.csv | parallel "csvsql --db postgresql:///hcid-disp  --insert {}"
 
 # remove output files
 rm ./$DIR_NAME/*_out.csv
-rm ./%DIR_NAME/*_err.csv
+rm ./$DIR_NAME/*_err.csv
 
 # create a view with all APNs in the same rows. Full outer join style
-psql -d hcid-disp -a -f ./etl/join.sql
+#psql -d hcid-disp -a -f ./etl/join.sql
 
