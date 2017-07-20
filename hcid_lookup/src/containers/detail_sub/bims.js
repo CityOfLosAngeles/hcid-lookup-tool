ad7edc7ebf6363b6ab92282c076cccc42e9c1d9e
Ben Chang <benjinchang@gmail.com>
2017-07-20 16:00:18 -0700

    finished formatting detail page

--- hcid_lookup/src/containers/detail_sub/bims.js
+++ hcid_lookup/src/containers/detail_sub/bims.js
@@ -15,38 +15,37 @@ class Bims extends Component {
 		const data = this.props.selected;
 
 		return(
-			<div className="hims_data">
-				<p className="detail-title">BIMS Data</p>
-				<table className="responsive-table striped centered bordered">
-					<thead>
-					     <tr>
-					     	 <th>Property Address</th>
-					         <th>Statement Date</th>
-					         {/* <th>APN</th> */}
-					         <th>RSO Exemptions</th>
-					         <th>SCEP Exemptions</th>
-					         <th>Total Units</th>
-					         <th>RSO Units Billed</th>
-					         <th>SCEP Units Billed</th>
-					     </tr>
-					</thead>
-
-
-
-					<tbody>
-						<tr>
-							<td>{data.Property_Address} {data.Property_City_State_Zip}</td>
-							<td>{data.StatementDate}</td>
-							{/* <td>{data.APN}</td> */}
-							<td>{data.RSO_Exemptions}</td>
-							<td>{data.SCEP_Exmpetions}</td>
-							<td>{data.Total_Units}</td>
-							<td>{data.RSO_Units_Billed}</td>
-							<td>{data.SCEP_Units_Billed}</td>
-				    	</tr>
-					</tbody>
-				</table>
+			<div className="detail-data">
+				<h4 className="detail-datatable-title">BIMS</h4>
+				<p className="detail-info-title">Property Address: 
+					<span className="detail-info">{data.Property_City_State_Zip}</span>
+				</p>
+				<p className="detail-info-title">Statement Date: 
+					<span className="detail-info">{data.StatementDate}</span>
+				</p>
+				<p className="detail-info-title">APN: 
+					<span className="detail-info">{data.APN}</span>
+				</p>
+				<p className="detail-info-title">RSO Exemptions: 
+					<span className="detail-info">{data.RSO_Exemptions}</span>
+				</p>
+				<p className="detail-info-title">SCEP Exemptions: 
+					<span className="detail-info">{data.SCEP_Exmpetions}</span>
+				</p>
+				<p className="detail-info-title">Total Units: 
+					<span className="detail-info">{data.Total_Units}</span>
+				</p>
+				<p className="detail-info-title">RSO Units Billed: 
+					<span className="detail-info">{data.RSO_Units_Billed}</span>
+				</p>
+				<p className="detail-info-title">SCEP Units Billed: 
+					<span className="detail-info">{data.SCEP_Units_Billed}</span>
+				</p>
+				<p className="detail-info-title">Data Source: 
+					<span className="detail-info">BIMS</span>
+				</p>
 			</div>
+			
 		);
 	}
 }
@@ -58,3 +57,34 @@ function mapStatetoProps(state){
 }
 
 export default connect(mapStatetoProps)(Bims);
+
+/*
+<div className="hims_data">
+				<p className="detail-title">BIMS Data</p>
+				<table className="responsive-table striped centered bordered">
+					<thead>
+					     <tr>
+					     	 <th>Property Address</th>
+					         <th>Statement Date</th>
+					          <th>APN</th> 
+					         <th>RSO Exemptions</th>
+					         <th>SCEP Exemptions</th>
+					         <th>Total Units</th>
+					         <th>RSO Units Billed</th>
+					         <th>SCEP Units Billed</th>
+					     </tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>{data.Property_Address} {data.Property_City_State_Zip}</td>
+							<td>{data.StatementDate}</td>
+							 <td>{data.APN}</td> 
+							<td>{data.RSO_Exemptions}</td>
+							<td>{data.SCEP_Exmpetions}</td>
+							<td>{data.Total_Units}</td>
+							<td>{data.RSO_Units_Billed}</td>
+							<td>{data.SCEP_Units_Billed}</td>
+				    	</tr>
+					</tbody>
+				</table>
+			</div>*/
--- hcid_lookup/src/containers/detail_sub/prop_site_address.js
+++ hcid_lookup/src/containers/detail_sub/prop_site_address.js
@@ -15,12 +15,62 @@ class PropertySiteAddress extends Component {
 		const data = this.props.selected;
 
 		return(
-			<div className="hims_data">
+			<div className="detail-data">
+				<h4 className="detail-datatable-title">Property Site Address</h4>
+				<p className="detail-info-title">APN: 
+					<span className="detail-info">{data.Apn}</span>
+				</p>
+				<p className="detail-info-title">Council District: 
+					<span className="detail-info">{data.CouncilDistrict}</span>
+				</p>
+				<p className="detail-info-title">CenTract 2010: 
+					<span className="detail-info">{data.CenTract2010}</span>
+				</p>
+				<p className="detail-info-title">Community Name: 
+					<span className="detail-info">{data.CommunityName}</span>
+				</p>
+				<p className="detail-info-title">State Assembly District: 
+					<span className="detail-info">{data.StateAssemDist}</span>
+				</p>
+				<p className="detail-info-title">State Senator District: 
+					<span className="detail-info">{data.StateSenDist}</span>
+				</p>
+				<p className="detail-info-title">US Congress District: 
+					<span className="detail-info">{data.UsCongDist}</span>
+				</p>
+				<p className="detail-info-title">County Sup District #: 
+					<span className="detail-info">{data.CountySupDistNum}</span>
+				</p>
+				<p className="detail-info-title">Community Plan Area ID: 
+					<span className="detail-info">{data.CommunityPlanAreaID}</span>
+				</p>
+				<p className="detail-info-title">Neighborhood Council ID: 
+					<span className="detail-info">{data.NeighborhoodCouncilID}</span>
+				</p>
+				<p className="detail-info-title">Data Source: 
+					<span className="detail-info">Property Site</span>
+				</p>
+			</div>
+			
+		);
+	}
+}
+
+function mapStatetoProps(state){
+	return{
+		selected : state.addresses.selected.Prop_site_addresses[0]
+	};
+}
+
+export default connect(mapStatetoProps)(PropertySiteAddress);
+
+
+{/*<div className="hims_data">
 				<p className="detail-title">Property Site Address Data</p>
 				<table className="responsive-table striped bordered">
 					<thead>
 						<tr>
-							{/* <th>APN</th> */}
+							{ <th>APN</th> }
 							<th>Council District</th>
 							<th>CenTract 2010</th>
 							<th>Community Name</th>
@@ -34,7 +84,7 @@ class PropertySiteAddress extends Component {
 					</thead>
 					<tbody>
 						<tr>
-							{/* <td>{data.Apn}</td> */}
+							{<td>{data.Apn}</td> }
 							<td>{data.CouncilDistrict}</td>
 							<td>{data.CenTract2010}</td>
 							<td>{data.CommunityName}</td>
@@ -47,15 +97,4 @@ class PropertySiteAddress extends Component {
 						</tr>
 					</tbody>
 				</table>
-			</div>
-		);
-	}
-}
-
-function mapStatetoProps(state){
-	return{
-		selected : state.addresses.selected.Prop_site_addresses[0]
-	};
-}
-
-export default connect(mapStatetoProps)(PropertySiteAddress);
+			</div>*/}
