ab93f420cc2fcc7d82543c9bbbeae42a57ecdaea
Ben Chang <benjinchang@gmail.com>
2017-08-03 17:24:30 -0700

    update config for heroku

--- models/index.js
+++ models/index.js
@@ -10,7 +10,9 @@ const config    = require(__dirname + '/../config/config.json')[env];
 let db        = {};
 
 if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+  var sequelize = new Sequelize(process.env[config.use_env_variable], {
+      dialect: "postgres"
+    });
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
