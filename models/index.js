5628dee2d90499d952f8436c51bb2ff46857ae8e
Ben Chang <benjinchang@gmail.com>
2017-08-03 17:17:23 -0700

    update heroku deployment

--- models/index.js
+++ models/index.js
@@ -6,11 +6,11 @@ const Sequelize = require('sequelize');
 const basename  = path.basename(module.filename);
 const env       = process.env.NODE_ENV || 'development';
 const config    = require(__dirname + '/../config/config.json')[env];
-const configDB  = require(__dirname + '/../config/database.js');
+// const configDB  = require(__dirname + '/../config/database.js');
 let db        = {};
 
-if (configDB.url) {
-  var sequelize = new Sequelize(process.env[configDB.url]);
+if (config.use_env_variable) {
+  var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
